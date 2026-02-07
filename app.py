4e9c16cea769307647b88eb2b18eca9487828106
nullgridslabs-ux <nullgridslabs@gmail.com>
2026-02-08 01:39:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,6 @@ def callback():
     return "CTF{easy_open_redirect}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
